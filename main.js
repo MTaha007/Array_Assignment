059ac5442cf858374fcdf6a10b0666c4227d69fd
MTaha007 <160823241+MTaha007@users.noreply.github.com>
2024-05-23 10:48:13 +0500

    array assignment

---
+++ main.js
@@ -0,0 +1,38 @@
+//Question 1:
+var fruits = ["Banana ", "Apple", "Mango", "Orange"];
+console.log(fruits);
+//Question 2
+var number = [10, 20, 30, 40];
+console.log(number);
+//Question 3
+var thirdfruit = fruits[3];
+console.log(thirdfruit);
+// //Question 4
+number[1] = 25;
+console.log(number);
+//Question 5
+fruits.push("Grape");
+console.log(fruits);
+//Question 6
+var lastfruit = fruits.pop();
+console.log(lastfruit);
+//Question 7
+var firstfruit = fruits.shift();
+console.log(firstfruit);
+//Question 8
+fruits.unshift("Kiwi");
+console.log(fruits);
+//Question 9
+fruits.shift();
+fruits.shift();
+console.log(fruits);
+//Question 10
+fruits.unshift("pineapple");
+fruits.unshift("pear");
+console.log(fruits);
+//Question 11
+var citrusFruits = fruits.slice(0, 2);
+console.log(citrusFruits);
+//Question 12
+var lastTwoFruits = fruits.slice(2);
+console.log(lastTwoFruits);
---
+++ main.ts
@@ -0,0 +1,54 @@
+//Question 1:
+let fruits:string[]=["Banana ","Apple","Mango","Orange"];
+console.log(fruits);
+
+
+//Question 2
+let number:number[]=[10,20,30,40]
+console.log(number);
+
+
+//Question 3
+let thirdfruit=fruits[3]
+ console.log(thirdfruit)
+
+
+// //Question 4
+ number[1]=25
+ console.log(number)
+
+
+ //Question 5
+ fruits.push("Grape");
+ console.log(fruits);
+
+
+//Question 6
+ let lastfruit=fruits.pop();
+ console.log(lastfruit);
+
+
+//Question 7
+let firstfruit=fruits.shift();
+console.log(firstfruit);
+
+
+//Question 8
+fruits.unshift("Kiwi");
+console.log(fruits);
+
+
+//Question 9
+fruits.shift()
+fruits.shift()
+console.log(fruits);
+
+
+//Question 10
+fruits.unshift("pineapple");
+fruits.unshift("pear");
+console.log(fruits);
+
+
+
+
